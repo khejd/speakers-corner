4cd5ec602b21aeb02adc8f661e77f6d6ba98a0d0
khejd <khejd@hotmail.com>
2018-01-30 12:56:24 +0100

    added css file and other minor changes

--- html/index.php
+++ html/index.php
@@ -1,1 +1,16 @@
-<? Php phpinfo ();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/app.css">
+    <title>Speakers-corner test site</title>
+</head>
+<body>
+<div class="input">
+    <form>
+        <input type="text" id="txtSearch" x-webkit-speech />
+        <button type="submit">Send inn</button>
+    </form>
+</div>
+</body>
+</html>
