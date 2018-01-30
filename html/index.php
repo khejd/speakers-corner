83990c3065606d91bfb1e1b68f1fcfa709c5936e
khejd <khejd@hotmail.com>
2018-01-30 13:49:13 +0100

    database config and form connection

--- html/index.php
+++ html/index.php
@@ -1,16 +1,41 @@
+include('../Connections/connection.php');
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/app.css">
     <title>Speakers-corner test site</title>
+
+
+
 </head>
 <body>
+
+
+<?php
+
+if(isset($_POST['save']))
+{
+    $sql = "INSERT INTO comment (user_id, comment)
+    VALUES ('".$_POST["user_id"]."','".$_POST["comment"]."')";
+
+    $result = mysqli_query($conn,$sql);
+}
+
+?>
+
 <div class="input">
-    <form>
-        <input type="text" id="txtSearch" x-webkit-speech />
+    <form action ="index.php" method="post">
+        <select name="user"> <!-- Need back-end connection here -->
+            <option value="1">User 1</option>
+        </select>
+        <input type="text" name="comment" x-webkit-speech />
         <button type="submit">Send inn</button>
     </form>
 </div>
+
+
+
 </body>
 </html>
