c207f9d9a5781268d8ef30aa2f166659894bc3d6
khejd <khejd@hotmail.com>
2018-02-01 15:59:56 +0100

    GET variables fix

--- html/codeHandler.php
+++ html/codeHandler.php
@@ -6,15 +6,15 @@ if (isset($_POST['submit'])){
     $phone = $_GET['phone'];
     $code = $_POST['code'];
 
-    $sql = "SELECT * FROM user WHERE phone_number = $phone ";
+    $sql = "SELECT * FROM user WHERE phone_number = $phone";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
     $id = $row['id'];
 
     if ($row['code'] == $code){
-        $sql = "UPDATE user SET 'verified' = 1 WHERE `user` . `id` = $id";
+        $sql = "UPDATE user SET verified = 1 WHERE id = $id";
         $result = mysqli_query($conn, $sql);
-        header('Location: index.php?phone='.$phone.'?user=verified');
+        header('Location: index.php?phone='.$phone.'&user=1');
     } else {
         header('Location: index.php');
     }
--- html/commentHandler.php
+++ html/commentHandler.php
@@ -5,7 +5,7 @@ if (isset($_POST['submit'])){
     $comment = $_POST['comment'];
     $phone = $_GET['phone'];
 
-    $sql = "SELECT * FROM user WHERE phone_number = $phone ";
+    $sql = "SELECT * FROM user WHERE phone_number = $phone";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
 
