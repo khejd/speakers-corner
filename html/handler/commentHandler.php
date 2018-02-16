212e8555d03bd4c1abf874baaee89e034f6ba2cd
khejd <khejd@hotmail.com>
2018-02-16 10:02:26 +0100

    minor changes

--- html/handler/commentHandler.php
+++ html/handler/commentHandler.php
@@ -4,7 +4,7 @@ include_once("../../Connections/connection.php");
 
     $phone = $_GET['phone'];
 
-    $sql = "SELECT * FROM `user` WHERE phone_number = $phone";
+    $sql = "SELECT * FROM `user` WHERE `phone_number` = $phone";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
 
--- html/handler/phoneHandler.php
+++ html/handler/phoneHandler.php
@@ -6,13 +6,10 @@ if (isset($_POST['submit'])){
     $stmt = $conn->prepare("INSERT INTO `user` (`phone_number`, `code`) VALUES (?, ?)");
     $stmt->bind_param("ii", $phone, $code);
 
-    $x = 4; // Amount of digits
-    $min = pow(10,$x);
-    $max = pow(10,$x+1)-1;
-    $code = rand($min, $max);
-
+    $code = rand(1000, 9999);
     $phone = $_POST['phone'];
     $comment = $_POST['comment'];
+
     $stmt->execute();
     $stmt->close();
 
