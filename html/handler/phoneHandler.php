e903642dec69dc2983a0eed20f85c8efc55c58ad
khejd <khejd@hotmail.com>
2018-02-16 09:53:59 +0100

    send comment in url

--- html/handler/phoneHandler.php
+++ html/handler/phoneHandler.php
@@ -3,16 +3,16 @@ include_once("../../Connections/connection.php");
 
 if (isset($_POST['submit'])){
 
-    $x = 4; // Amount of digits
-    $min = pow(10,$x);
-    $max = pow(10,$x+1)-1;
-
-
     $stmt = $conn->prepare("INSERT INTO `user` (`phone_number`, `code`) VALUES (?, ?)");
     $stmt->bind_param("ii", $phone, $code);
 
-    $phone = $_POST['phone'];
+    $x = 4; // Amount of digits
+    $min = pow(10,$x);
+    $max = pow(10,$x+1)-1;
     $code = rand($min, $max);
+
+    $phone = $_POST['phone'];
+    $comment = $_POST['comment'];
     $stmt->execute();
     $stmt->close();
 
