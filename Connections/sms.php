b4f1ecd8a35bfedfb0098c7c5f781576fedce87e
Karl Henrik Ejdfors <khejd@hotmail.com>
2018-02-18 16:22:22 +0100

    Update sms.php

--- Connections/sms.php
+++ Connections/sms.php
@@ -1,8 +1,8 @@
 <?php
 include "smsGateway.php";
 
-$smsGateway = new SmsGateway('khejd@smsgateway.me', 'pQbmCT9E');
-$deviceID = '79139';
+$smsGateway = new SmsGateway('username@smsgateway.me', 'password');
+$deviceID = 'id';
 
 // Documentation: https://smsgateway.me/sms-api-documentation/getting-started
 
