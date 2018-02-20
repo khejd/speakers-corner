be620583b20231115ee4b115f24098a168d4f388
khejd <khejd@hotmail.com>
2018-02-20 18:18:30 +0100

    include jQuery

--- html/comments.php
+++ html/comments.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/app.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../js/bootstrap.min.js"></script>
     <title>Speakers-corner test site</title>
