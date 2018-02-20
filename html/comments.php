c585b66673c0bc1a2f8f70a95354fc351b36a352
khejd <khejd@hotmail.com>
2018-02-20 18:43:01 +0100

    vote-id

--- html/comments.php
+++ html/comments.php
@@ -55,7 +55,7 @@
             type: 'POST',
             data: {id: id},
             success: function(data){
-                $('#vote' + id).text(parseInt($('#vote' + id).text())+1);
+                $('#vote-' + id).text(parseInt($('#vote-' + id).text())+1);
             }
         })
     }
@@ -66,7 +66,7 @@
             type: 'POST',
             data: {id: id},
             success: function (data) {
-                $('#vote' + id).text(parseInt($('#vote' + id).text())-1);
+                $('#vote-' + id).text(parseInt($('#vote-' + id).text())-1);
             }
         })
     }
