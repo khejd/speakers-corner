d6ad82fc693d9b0d9ee2ba3d2af27164061047c1
khejd <khejd@hotmail.com>
2018-02-22 12:31:04 +0100

    cookie changes

--- html/handler/downVoteHandler.php
+++ html/handler/downVoteHandler.php
@@ -2,24 +2,38 @@
 include_once("../../Connections/connection.php");
 
     $id = $_POST['id'];
-
     $cookie_name = "vote";
-    $comment_id = array(
-        "comments" => array(
-            "id" => $id,
-            "vote" => 0
-        )
+    $new_entry = array(
+        'id' => $id,
+        'vote' => 0
     );
-    $cookie_value = $comment_id;
+
+    if(!isset($_COOKIE)){
+        $cookie_value = array();
+        array_push($cookie_value, $new_entry);
+
+    } else {
+        $cookie_value = json_decode($_COOKIE[$cookie_name], true);
+        $key = array_search($id, array_column($cookie_value, 'id'));
+        if ($cookie_value[$key]['id'] == $id){
+            $cookie_value[$key]['vote'] = 0;
+        } else {
+            array_push($cookie_value, $new_entry);
+        }
+
+    }
 
     setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/"); // 86400 = expires in one day
 
-    if($_COOKIE[$cookie_value.$comment_id[$id]->vote] == 1){
+    $data = json_decode($_COOKIE[$cookie_name], true);
+    $key = array_search($id, array_column($data, 'id'));
+
+    if($data[$key]['vote'] == 1){
         $sql = "UPDATE `comment` SET `vote` = `vote` - 1 WHERE `comment`.`id` = $id";
         $result = mysqli_query($conn, $sql);
     }
 
-    if($_COOKIE[$cookie_value.$comment_id[$id]->vote] == 0){
+    if($data[$key]['vote'] == 0){
         $sql = "UPDATE `comment` SET `vote` = `vote` + 1 WHERE `comment`.`id` = $id";
         $result = mysqli_query($conn, $sql);
     }
--- html/handler/upVoteHandler.php
+++ html/handler/upVoteHandler.php
@@ -2,25 +2,38 @@
 include_once("../../Connections/connection.php");
 
     $id = $_POST['id'];
-
     $cookie_name = "vote";
-    $comment_id = array(
+    $new_entry = array(
         'id' => $id,
         'vote' => 1
-
     );
-    $cookie_value = $comment_id;
+
+    if(!isset($_COOKIE)){
+        $cookie_value = array();
+        array_push($cookie_value, $new_entry);
+
+    } else {
+        $cookie_value = json_decode($_COOKIE[$cookie_name], true);
+        $key = array_search($id, array_column($cookie_value, 'id'));
+        if ($cookie_value[$key]['id'] == $id){
+            $cookie_value[$key]['vote'] = 1;
+        } else {
+            array_push($cookie_value, $new_entry);
+        }
+
+    }
 
     setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/"); // 86400 = expires in one day
 
-    var_dump($_COOKIE[$cookie_value.$comment_id[$id]->vote]);
+    $data = json_decode($_COOKIE[$cookie_name], true);
+    $key = array_search($id, array_column($data, 'id'));
 
-    if($_COOKIE[$cookie_value.$comment_id[$id]->vote] == 0){
+    if($data[$key]['vote'] == 0){
         $sql = "UPDATE `comment` SET `vote` = `vote` + 1 WHERE `comment`.`id` = $id";
         $result = mysqli_query($conn, $sql);
     }
 
-    if($_COOKIE[$cookie_value.$comment_id[$id]->vote] == 1){
+    if($data[$key]['vote'] == 1){
         $sql = "UPDATE `comment` SET `vote` = `vote` - 1 WHERE `comment`.`id` = $id";
         $result = mysqli_query($conn, $sql);
     }
