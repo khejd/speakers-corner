7a4df1040227ca471ed837c9b70fe913cd02adf6
Karl Henrik Ejdfors <khejd@hotmail.com>
2018-02-22 14:40:02 +0100

    Votes (#17)
    * up/down vote and fontawesome
    
    * minor changes
    
    * console logging
    
    * location -> pushstate
    
    * ajax communication with php
    
    * include jQuery
    
    * handlers->handler
    
    * upVote->downVote
    
    * update votes value
    
    * parseInt
    
    * vote-id
    
    * use cookies
    
    * new logic for cookies
    
    * cookie update
    
    * remove while loop
    
    * cookie changes
    
    * $_COOKIE -> $_COOKIE['name']
    
    * vote states
    
    * minor changes
    
    * very small change
    
    * jQuery up/down update
    
    * jQuery up/down update

--- html/comments.php
+++ html/comments.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../css/app.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="../js/bootstrap.min.js"></script>
     <title>Speakers-corner test site</title>
 
@@ -26,7 +28,14 @@
                         <tbody>";
 
                 while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-                    echo "<tr><td>" . $row['text'] . "</td><td>" . $row['vote'] . "</td></tr>";
+                    $id = $row['id'];
+                    echo "<tr>
+                            <td>" . $row['text'] . "</td>
+                            <td>
+                                <span onClick='upVote(".$id.")'><i class='fa fa-angle-up'></i></span>
+                                <span id='vote-".$id."'>" . $row['vote'] . "</span>
+                                <span onClick='downVote(".$id.")'><i class='fa fa-angle-down'></i></span>
+                          </tr>";
                 }
 
                 echo "</tbody>
@@ -38,4 +47,35 @@
     </div>
 </div>
 </body>
+
+<script>
+
+    function upVote(id) {
+        $.ajax({
+            url: "handler/upVoteHandler.php",
+            type: 'POST',
+            data: {id: id},
+            success: function(result){
+                if (result == 'up'){
+                    $('#vote-' + id).text(parseInt($('#vote-' + id).text())+1);
+                }
+            }
+        })
+    }
+
+    function downVote(id) {
+        $.ajax({
+            url: "handler/downVoteHandler.php",
+            type: 'POST',
+            data: {id: id},
+            success: function (result) {
+                if (result == 'down'){
+                    $('#vote-' + id).text(parseInt($('#vote-' + id).text())-1);
+                }
+            }
+        })
+    }
+</script>
+
+
 </html>
---
+++ html/handler/upVoteHandler.php
@@ -0,0 +1,56 @@
+<?php
+include_once("../../Connections/connection.php");
+
+    $id = $_POST['id'];
+    $cookie_name = "vote";
+    $new_entry = array(
+        'id' => $id,
+        'vote' => 'up'
+    );
+
+    if(!isset($_COOKIE[$cookie_name])){
+        $cookie_value = array();
+        array_push($cookie_value, $new_entry);
+    } else {
+        $cookie_value = json_decode($_COOKIE[$cookie_name], true);
+        $key = array_search($id, array_column($cookie_value, 'id'));
+
+        if ($cookie_value[$key]['id'] == $id){
+            if ($cookie_value[$key]['vote'] == 'down'){
+                $cookie_value[$key]['vote'] = 'none';
+            } else if ($cookie_value[$key]['vote'] == 'none'){
+                $cookie_value[$key]['vote'] = 'up';
+            }
+        } else {
+            array_push($cookie_value, $new_entry);
+        }
+
+    }
+
+    setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/"); // 86400 = expires in one day
+
+    $data = json_decode($_COOKIE[$cookie_name], true);
+    $key = array_search($id, array_column($data, 'id'));
+
+    if(isset($_COOKIE[$cookie_name])){
+        if($data[$key]['vote'] == 'none'){ // one reload behind
+            $sql = "UPDATE `comment` SET `vote` = `vote` + 1 WHERE `comment`.`id` = $id";
+            $result = mysqli_query($conn, $sql);
+            echo 'up';
+        }
+
+        if($data[$key]['vote'] == 'down'){
+            $sql = "UPDATE `comment` SET `vote` = `vote` + 1 WHERE `comment`.`id` = $id";
+            $result = mysqli_query($conn, $sql);
+            $cookie_value[$key]['vote'] = 'none';
+            setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/");
+            echo 'up';
+        }
+    } else {
+        $sql = "UPDATE `comment` SET `vote` = `vote` + 1 WHERE `comment`.`id` = $id";
+        $result = mysqli_query($conn, $sql);
+        echo 'up';
+    }
+
+
+?>
