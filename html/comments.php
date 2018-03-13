f44ee322e38a5a58fe7b091f3e1fd7cf2fc6dfd2
volrath <volrath-bugge@hotmail.com>
2018-03-13 15:31:16 +0100

    test sorting

--- html/comments.php
+++ html/comments.php
@@ -69,7 +69,7 @@
 
                 
 
-                echo "<table class='table table-hover'>
+                echo "<table class='table table-hover table-sort'>
                         <tbody>";
 
                 foreach ($newArray as $row){   //Creates a loop to loop through results
@@ -126,7 +126,7 @@ function sortBy(argument)
     attrs: {class: 'table'}
     });
 
-$('body').replaceWith(table);
+$('table-sort').replaceWith(table);
 }
 
 
