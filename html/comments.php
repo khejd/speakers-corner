d384bc8984289664113e30a92f0487f7da23a56e
volrath <volrath-bugge@hotmail.com>
2018-03-16 11:13:36 +0100

    error fix

--- html/comments.php
+++ html/comments.php
@@ -116,7 +116,7 @@ function sortBy(argument)
     {
         ar.sort(function(a,b)
         {
-            return (parsInt(a['ups'],10)-parsInt(a['downs'],10)) - (parsInt(b['ups'],10)-parsInt(b['downs'],10));
+            return (Number(a['ups'])-Number(a['downs'])) - (Number(b['ups'],10)-Number(b['downs'],10));
         }
         );
     }
