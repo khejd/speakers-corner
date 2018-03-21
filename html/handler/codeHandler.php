650400ab6823b5d0bd111688643d5e46ffed0e5f
khejd <khejd@hotmail.com>
2018-03-21 20:48:03 +0100

    bug found (intval..), if code does not match -> warn user

--- html/handler/codeHandler.php
+++ html/handler/codeHandler.php
@@ -9,12 +9,16 @@ include_once("../../Connections/connection.php");
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
 
-    $stmt = $conn->prepare("INSERT INTO `comment` (`user_id`, `text`) VALUES (?, ?)");
-    $stmt->bind_param("is", $user_id, $comment);
+    if ($code == $row['code']){
+        $stmt = $conn->prepare("INSERT INTO `comment` (`user_id`, `text`) VALUES (?, ?)");
+        $stmt->bind_param("is", $user_id, $comment);
 
-    $user_id = $row['id'];
-    $stmt->execute();
-    $stmt->close();
+        $user_id = $row['id'];
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        echo "error";
+    }
 
     exit;
 
