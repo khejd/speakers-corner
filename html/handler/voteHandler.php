e160efde8a20b77bdc49d613866f9dd14617499d
khejd <khejd@hotmail.com>
2018-03-22 22:49:34 +0100

    cookie hunt

--- html/handler/voteHandler.php
+++ html/handler/voteHandler.php
@@ -12,20 +12,7 @@
     if(!isset($_COOKIE[$cookie_name])){
         $cookie_value = array();
         array_push($cookie_value, $new_entry);
-    } else {
-        $cookie_value = json_decode($_COOKIE[$cookie_name], true);
 
-        if (!in_array($id, array_column($cookie_value, 'id'))){
-            array_push($cookie_value, $new_entry);
-        }
-
-    }
-
-    setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/"); // 86400 = expires in one day
-
-    $data = json_decode($_COOKIE[$cookie_name], true);
-
-    if (isset($_COOKIE[$cookie_name]) && !in_array($id, array_column($data, 'id'))) {
         if ($action == 'up') {
             $sql = "UPDATE `comment` SET `ups` = `ups` + 1 WHERE `comment`.`id` = $id";
             $result = mysqli_query($conn, $sql);
@@ -33,6 +20,22 @@
             $sql = "UPDATE `comment` SET `downs` = `downs` - 1 WHERE `comment`.`id` = $id";
             $result = mysqli_query($conn, $sql);
         }
+    } else {
+        $cookie_value = json_decode($_COOKIE[$cookie_name], true);
+
+        if (!in_array($id, array_column($cookie_value, 'id'))){
+            array_push($cookie_value, $new_entry);
+            if ($action == 'up') {
+                $sql = "UPDATE `comment` SET `ups` = `ups` + 1 WHERE `comment`.`id` = $id";
+                $result = mysqli_query($conn, $sql);
+            } else if ($action == 'down') {
+                $sql = "UPDATE `comment` SET `downs` = `downs` - 1 WHERE `comment`.`id` = $id";
+                $result = mysqli_query($conn, $sql);
+            }
+        }
+
     }
+
+    setcookie($cookie_name, json_encode($cookie_value), time() + 86400, "/"); // 86400 = expires in one day
 
 ?>
