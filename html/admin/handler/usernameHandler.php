50698e0edcb600690324c064f29155f80eb5b8c9
khejd <khejd@hotmail.com>
2018-04-06 16:38:04 +0200

    logic changes

--- html/admin/handler/usernameHandler.php
+++ html/admin/handler/usernameHandler.php
@@ -4,19 +4,19 @@ include_once("../../Connections/connection.php");
 function usernameExists($username, $conn){
     $sql = "SELECT * FROM `admin` WHERE `username` = '$username'";
     $result = mysqli_query($conn, $sql);
-    return mysqli_num_rows($result) == 1;
+    return mysqli_num_rows($result) > 0;
 }
 
 function phoneExists($phone, $conn){
     $sql = "SELECT * FROM `admin` WHERE `phone` = '$phone'";
     $result = mysqli_query($conn, $sql);
-    return mysqli_num_rows($result) == 1;
+    return mysqli_num_rows($result) > 0;
 }
 
 $username = $_POST['username'];
 $phone = $_POST['phone'];
 
-if (usernameExists($username, $conn) || phoneExists($phone, $conn)){
+if (!usernameExists($username, $conn) && !phoneExists($phone, $conn)){
     echo json_encode(array(
         'error' => false
     ));
