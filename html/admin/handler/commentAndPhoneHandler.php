efb5013017a83eba33b9940e8ed2b53e502cad18
khejd <khejd@hotmail.com>
2018-04-06 11:14:20 +0200

    phone undefined

--- html/admin/handler/commentAndPhoneHandler.php
+++ html/admin/handler/commentAndPhoneHandler.php
@@ -2,13 +2,13 @@
 include_once("../../Connections/connection.php");
 
 function getComments($conn){
-    $sql = "SELECT * FROM `comment`";
+    $sql = "SELECT * FROM `comment` ORDER BY TIME";
     return mysqli_query($conn, $sql);
 }
 
 function getPhone($comment, $conn){
     $id = $comment['user_id'];
-    $sql = "SELECT * FROM `user` WHERE `id` = $id";
+    $sql = "SELECT * FROM `user` WHERE `id` = '$id'";
     return mysqli_query($conn, $sql);
 }
 
@@ -16,8 +16,8 @@ $comments = getComments($conn);
 $array = array();
 
 while ($comment = mysqli_fetch_array($comments)){
-    $phone = getPhone($comment, $conn);
-    array_push($array, $comment, $phone);
+    $comment['phone'] = getPhone($comment, $conn);
+    array_push($array, $comment);
 }
 
 echo json_encode($array);
