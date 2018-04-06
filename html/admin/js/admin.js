b60ba4246f57d1591779a87d0c8312bec52e7746
khejd <khejd@hotmail.com>
2018-04-06 11:40:11 +0200

    order by time desc

--- html/admin/js/admin.js
+++ html/admin/js/admin.js
@@ -1,35 +1,38 @@
-// Check if logged in
-$.ajax({
-    url: 'handler/loggedInHandler.php',
-    type: 'GET',
-    success: (result) => {
-        let res = $.parseJSON(result);
-        if (res.error){
-            window.location.href = 'index.html'
-        } else {
-        $('body').css('display', 'block');
-        }
-    }
-});
-
-// Logout
-$('#logout').on('click', () => {
-    $.ajax({
-        url: 'handler/logoutHandler.php',
-        success: () => {
-            window.location.href = 'index.html'
-        }
-    });
-});
-
 // On document ready
 $(() => {
-    // Fetch comments
+    // Check if logged in
+    $.ajax({
+        url: 'handler/loggedInHandler.php',
+        type: 'GET',
+        success: (result) => {
+            let res = $.parseJSON(result);
+            if (res.error){
+                window.location.href = 'index.html'
+            } else {
+                $('body').css('display', 'block');
+            }
+        }
+    });
+
     let comments = '';
     let deferred = getCommentAndPhone();
 
-    deferred.then(() =>{
+    // When all comments are fetched
+    deferred.then(() => {
         printTable(comments);
+
+
+
+        // Logout
+        $('#logout').on('click', () => {
+            $.ajax({
+                url: 'handler/logoutHandler.php',
+                success: () => {
+                    window.location.href = 'index.html'
+                }
+            });
+        });
+
     });
 });
 
