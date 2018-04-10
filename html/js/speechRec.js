75b016a38184bae647b99f22d60af94164b380bb
unknown <nhathb@stud.ntnu.no>
2018-04-10 15:49:37 +0200

    removed some lines

--- html/js/speechRec.js
+++ html/js/speechRec.js
@@ -28,24 +28,6 @@ $('.flag').on('click', (e) => {
     localStorage.setItem("language", language);
 });
 
-window.requestAnimationFrame = (function(){
-  return  window.requestAnimationFrame       ||
-          window.webkitRequestAnimationFrame ||
-          window.mozRequestAnimationFrame    ||
-          function( callback ){
-            window.setTimeout(callback, 1000 / 60);
-          };
-})();
-
-var speed = 5000;
-(function currencySlide(){
-    var currencyPairWidth = $('.slideItem:first-child').outerWidth();
-    $(".slideContainer").animate({marginLeft:-currencyPairWidth},speed, 'linear', function(){
-                $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-        });
-        requestAnimationFrame(currencySlide);
-})();
-
 // Speech recognition variables
 let active = false;
 let recognition = '';
