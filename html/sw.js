8a7cd26764503d396d30ec7faf6233d298dd8edb
khejd <khejd@hotmail.com>
2018-04-17 21:12:26 +0200

    include font-awesome

--- html/sw.js
+++ html/sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = 'speak-v1';
 let urlsToCache = [
     'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/css/flag-icon.min.css',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
     'index.html',
     '/static/css/app.css',
     '/phone/index.html',
