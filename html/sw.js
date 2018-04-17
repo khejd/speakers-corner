2b455b97c6bf3cc4424a8409606cf44431c87a47
khejd <khejd@hotmail.com>
2018-04-17 18:41:49 +0200

    sw

--- html/sw.js
+++ html/sw.js
@@ -1,15 +1,11 @@
 const CACHE_NAME = 'speak-v1';
 let urlsToCache = [
+    '/',
     '/static/',
-    '/comment/index.html',
-    '/phone/index.html',
-    '/speak/index.html',
+    '/phone/',
+    '/comment/',
     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/css/flag-icon.min.css',
-    'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
-    'https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js'
 ];
 
 self.addEventListener('install', (event) => {
