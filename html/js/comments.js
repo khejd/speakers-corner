d1ce758f9724ce976bc1589c60439fd55d6ceeaa
khejd <khejd@hotmail.com>
2018-04-17 13:25:17 +0200

    nav bar and jumbotron

--- html/js/comments.js
+++ html/js/comments.js
@@ -21,7 +21,7 @@ $(() => {
         }
 
         // Sort comments
-        $('.sort_selecter').on('click', (e) => {
+        $('.sort_selector').on('click', (e) => {
             let sorter = e.target.id;
             sortBy(sorter);
         });
