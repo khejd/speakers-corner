bf2964d1febe7cd02e4d62af303a8625c15b714f
khejd <khejd@hotmail.com>
2018-04-17 21:05:10 +0200

    sw

--- html/sw.js
+++ html/sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'speak-v1';
 let urlsToCache = [
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
     'https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.1.0/css/flag-icon.min.css',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
     'index.html',
     '/static/css/app.css',
     '/phone/index.html',
